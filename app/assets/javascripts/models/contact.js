bf885ddc1ffe6431d543c71337a0fd749e0e1772
Cyril Fluck <github@fluck.fr>
2013-02-28 16:40:23 -0800

    Activate the FixtureAdapter

--- app/assets/javascripts/models/contact.js
+++ app/assets/javascripts/models/contact.js
@@ -29,3 +29,9 @@ App.Contact  = DS.Model.extend({
     return 'http://www.gravatar.com/avatar/' + MD5(email);
   }.property('email')
 });
+
+App.Contact.FIXTURES = [{
+  id: 1,
+  firstName: 'John',
+  lastName: 'Doe'
+}];
